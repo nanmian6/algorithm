76b33afe9c8b765bf84e07f77291f83e231b9242
nanmian <nanmianya@qq.com>
2022-11-23 21:43:15 +0800

    实现最小栈

--- src/linked/GetMinStack.java
+++ src/linked/GetMinStack.java
@@ -1,13 +1,114 @@
 package linked;
 
+import java.util.Stack;
+
 /**
  * @author nanmian
  * 实现最小栈
+ * 实现一个特殊的栈，在基本功能的基础上，再实现返回栈中最小元素的功能
+ *
+ * 1）pop、push、getMin操作的时间复杂度都是 O(1)。
+ *
+ * 2）设计的栈类型可以使用现成的栈结构。
  */
 public class GetMinStack {
 
     public static class MyStack1{
+        private Stack<Integer> stackData;
+        private Stack<Integer> stackMin;
 
+        public MyStack1() {
+            this.stackData = new Stack<>();
+            this.stackMin = new Stack<>();
+        }
+
+        public void push(int newNum) {
+            if (this.stackMin.isEmpty()) {
+                this.stackMin.push(newNum);
+            } else if (newNum <= this.getMin()) {
+                this.stackMin.push(newNum);
+            }
+            this.stackData.push(newNum);
+        }
+
+        public int pop() {
+            if (this.stackData.isEmpty()) {
+                throw new RuntimeException("Your stack is empty!");
+            }
+            int value = this.stackData.pop();
+            if (value == this.getMin()) {
+                this.stackMin.pop();
+            }
+            return value;
+        }
+
+        public int getMin() {
+            if (this.stackMin.isEmpty()) {
+                throw new RuntimeException("Your stack is empty!");
+            }
+            return this.stackMin.peek();
+        }
+    }
+
+
+    public static class MyStack2{
+        private Stack<Integer> stackData;
+        private Stack<Integer> stackMin;
+
+        public MyStack2() {
+            this.stackData = new Stack<>();
+            this.stackMin = new Stack<>();
+        }
+
+        public void push(int newNum) {
+            if (this.stackMin.isEmpty()) {
+                this.stackMin.push(newNum);
+            } else if (newNum < this.getMin()) {
+                this.stackMin.push(newNum);
+            } else {
+                this.stackMin.push(this.stackMin.peek());
+            }
+            this.stackData.push(newNum);
+        }
+
+        public int pop() {
+            if (this.stackData.isEmpty()) {
+                throw new RuntimeException("Your stack is empty!");
+            }
+            this.stackMin.pop();
+            return this.stackData.pop();
+        }
+
+        public int getMin() {
+            if (this.stackMin.isEmpty()) {
+                throw new RuntimeException("Your stack is empty!");
+            }
+            return this.stackMin.peek();
+        }
+    }
+
+    public static void main(String[] args) {
+        MyStack1 stack1 = new MyStack1();
+        stack1.push(3);
+        System.out.println(stack1.getMin());
+        stack1.push(4);
+        System.out.println(stack1.getMin());
+        stack1.push(1);
+        System.out.println(stack1.getMin());
+        System.out.println(stack1.pop());
+        System.out.println(stack1.getMin());
+
+        System.out.println("=============");
+
+        MyStack1 stack2 = new MyStack1();
+        stack2.push(3);
+        System.out.println(stack2.getMin());
+        stack2.push(4);
+        System.out.println(stack2.getMin());
+        stack2.push(1);
+        System.out.println(stack2.getMin());
+        System.out.println(stack2.pop());
+        System.out.println(stack2.getMin());
     }
 
 }
